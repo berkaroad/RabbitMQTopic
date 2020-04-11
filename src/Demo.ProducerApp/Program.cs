bd614cb3f0f1df51d16ef9dc895fb6cc41d560a7
Jerry Bai <chinaren_bjr@163.com>
2020-04-11 21:35:08 +0800

    demo 调整

--- src/Demo.ProducerApp/Program.cs
+++ src/Demo.ProducerApp/Program.cs
@@ -14,11 +14,14 @@ namespace Demo.ProducerApp
             }, delayedMessageEnabled: true);
 
             producer.Start();
+            var random = new Random();
             for (var i = 1; i <= 10; i++)
             {
-                var body = System.Text.Encoding.UTF8.GetBytes($"delayed message {i}");
-                var topicMessage = new TopicMessage("CommandTopic", 4, 1, body, "text/json", "System.String", delayedMillisecond: 1000 * (15 - i));
-                producer.SendMessage(topicMessage, "routingKey", "messageId");
+                var messageId = Guid.NewGuid().ToString();
+                var routingKey = Guid.NewGuid().ToString();
+                var body = System.Text.Encoding.UTF8.GetBytes($"{i} delayed message {messageId}");
+                var topicMessage = new TopicMessage("CommandTopic", 4, 1, body, "text/json", "System.String", delayedMillisecond: 1000 * random.Next(5, 10));
+                producer.SendMessage(topicMessage, routingKey, messageId);
             }
             Console.WriteLine("Producer started!");
             Console.ReadLine();
